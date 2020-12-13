3739838833cc7f5fb1f419f200f41ffd216c301a
FernandoPFH <19.00499-0@maua.br>
2020-12-13 19:43:54 -0300

    HOTFIX 10

--- Servidor/ImagesDB/Settup.sh
+++
@@ -1,5 +0,0 @@
-IDBIP="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
-
-echo $IDBIP
-
-export IDBIP
--- Servidor/RemoveImages.bat
+++
@@ -1,3 +0,0 @@
-for /f "tokens=3" %%i in ('docker images') do (
-    docker rmi -f %%i
-)
