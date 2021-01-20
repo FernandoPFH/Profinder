26eab8b0b2f676b9691de5f79dd8d8cd7c029ab2
FernandoPFH <19.00499-0@maua.br>
2021-01-20 20:24:20 -0300

    HOTFIX para Testes HTTPS

---
+++ Site/server_conf.sh
@@ -0,0 +1,36 @@
+cd /home/azureuser
+
+git clone https://github.com/FernandoPFH/Profinder.git
+
+sudo apt update
+sudo apt install nginx
+
+sudo ufw allow 'Nginx Full'
+
+sudo ln -s /home/azureuser/Profinder/Site/Nginx/profinder.brazilsouth.cloudapp.azure.com /etc/nginx/sites-available/
+
+sudo ln -s /home/azureuser/Profinder/Site/Nginx/profinder.brazilsouth.cloudapp.azure.com /etc/nginx/sites-enabled/
+
+sudo nano /etc/nginx/nginx.conf
+
+sudo nginx -t
+
+sudo systemctl restart nginx
+
+sudo certbot --nginx -d profinder.brazilsouth.cloudapp.azure.com -d profinder.brazilsouth.cloudapp.azure.com
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+cd ./Profinder/Site
+
+sudo nano docker-compose.yml
+
+sudo docker-compose up -d --build
+
+#cd ../Servidor
+
+#sudo nano docker-compose.yml
+
+#sudo docker-compose up -d --build
