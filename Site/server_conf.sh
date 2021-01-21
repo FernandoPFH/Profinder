629093b213509c63ac053cb23b293d39dc34adef
FernandoPFH <19.00499-0@maua.br>
2021-01-20 23:42:23 -0300

    HOTFIX Versao do Docker-Compose

--- Site/server_conf.sh
+++ Site/server_conf.sh
@@ -17,7 +17,7 @@ sudo systemctl restart nginx
 
 sudo certbot --nginx -d profinder.brazilsouth.cloudapp.azure.com -d profinder.brazilsouth.cloudapp.azure.com
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
